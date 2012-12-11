cf58b2a09fbb8e269b12014a32641d6d4c7d3560
parhamfh <parhamfh@kth.se>
2012-12-11 15:54:43 +0900

    Did some good ol' refactr'n

---
+++ rhythm32/rhythm32.py
@@ -0,0 +1,207 @@
+#!/usr/local/bin/python
+
+import viterbi, numpy
+import random as ra
+from itertools import izip_longest
+
+from sounder import Sounder
+
+class BeatPair(object):
+    '''
+    Hidden state.
+    '''
+    def __init__(self, origin, to, idx):
+        self.orig = origin
+        self.dest = to
+        self.i = idx
+
+    @property
+    def origin(self):
+        return self.orig
+
+    @property
+    def to(self):
+        return self.dest
+
+    @property
+    def duration(self):
+        return self.to-self.origin+1
+
+    def __repr__(self):
+        return "{0}{3}({1},{2})".format('B', self.orig, self.dest, self.i)
+
+class Syllable(object):
+    '''
+    Output/emission.
+    '''
+
+    def __init__(self, syllable, duration, accent):
+        self.s = syllable
+        self.d = duration
+        self.e = accent
+
+    @property
+    def syllable(self):
+        return self.s
+    
+    @property
+    def duration(self):
+        return self.d
+
+    @property
+    def accent(self):
+        return self.e
+
+    @property
+    def stress(self):
+        raise RuntimeError("It is called ACCENT../")
+
+    def __repr__(self):
+        return "(\"{0}\",{1},{2})".format(self.s, self.d, self.e)
+
+DEBUG=False
+
+def dprint(s=""):
+    if DEBUG:
+        print "<DEBUG>",s
+
+def trans_p_strange(B, goalsum=1):
+    '''
+    Strange distribution
+    ===
+
+    Starts out (round 1) by assuming equal distribution amongst the transitions
+    to receive probabilities, calculates probability based on sum of probabilities 
+    being 1. After this round
+    it assumes equal distribution between remaining transitions to receive
+    probabilities, and calculates probability from 1-`previous probability`. 
+    And goes on till final transition probability remains which is given the 
+    remainder of what is left of 1. 
+
+    Is this anything real mathematical?
+    '''
+    T = numpy.zeros(shape=(len(B),len(B)))
+    for i in range(len(B)):
+        r = (range(i,len(B)))
+        tot = goalsum
+        divisors = len(r)
+        div = round(tot/divisors,4)
+        for j in r:
+            if divisors>1:
+                v = round(abs(ra.gauss(div,div/2)),4)
+                T[i][j]= v
+                tot -= v
+                divisors -= 1
+                div = tot/divisors
+            else:
+                T[i][j]=round(tot,4)
+    return T
+    
+def trans_p_gauss(B,goalsum=1):
+    T = numpy.zeros(shape=(len(B),len(B)))
+    for i in range(len(B)):
+        for j in range(i,len(B)):
+            v = ra.gauss(goalsum,goalsum)
+            T[i][j]= v
+        s = sum(T[i])/goalsum
+        T[i] = map(lambda x:x/s, T[i])
+    return T
+
+def trans_p_randnorm(B,val=1):
+    '''
+    generated values will have a summed value
+    equal/"equal" to param sum
+    '''
+    T = numpy.zeros(shape=(len(B),len(B)))
+    for b in B:
+        for k in B:
+            if b.to < k.origin:
+                T[b.i][k.i] = round(ra.random(),4)
+        su = sum(T[b.i])
+        if su != 0:
+            T[b.i] = [round((val*x)/su,4) for x in T[b.i]]
+
+    return T
+
+def generate_e_p(e_p, beats):
+    for b in beats:
+        sh = ra.random()
+        e_p[b.i] = {"UNSTRESSED":sh, "STRESSED": 1-sh}
+
+def generate_d_p(d_p, beats):
+    for b in beats:
+        sh = ra.random()
+        d_p[b.i] = {"SHORT":sh, "LONG": 1-sh}
+
+def accent_p(b, emission):
+    return e_p[b.i][emission.accent]
+
+def duration_p(b, emission):
+    return d_p[b.i][emission.duration]
+
+def print_beats(B):
+    pass
+## PLAY BALL ##
+
+num_beats=32
+B=[]
+
+#Only ring for the beats duration or this till the next designated state
+idx = 0
+for i in range(0,num_beats):
+    for j in range (i,num_beats):
+        B.append(BeatPair(i,j,idx))
+        idx += 1
+
+dprint(B)
+
+S = [Syllable("Tom","SHORT","UNSTRESSED"), Syllable("ten","LONG","STRESSED")]
+
+start_p = [1.0/len(B) for _ in B]
+dprint("\n start p:%s\n"%start_p)
+
+T = trans_p_randnorm(B,1)
+
+numpy.set_printoptions(suppress=True, precision=5)
+dprint("T: %s\n"%T)
+
+dprint("Sum of T's rows:")
+if DEBUG:
+    for row in range(0,len(T)):
+        dprint(sum(T[row]))
+
+# LOOK HERE
+e_p = range(len(B))
+generate_e_p(e_p, B)
+d_p = range (len(B))
+generate_d_p(d_p, B)
+
+dprint("\nProbabilities:")
+if DEBUG:
+    for b in B:  
+        dprint("{0} \nwith e={1} and d={2}".format(b,e_p[b.i],d_p[b.i],sum(e_p[b.i].values()),sum(d_p[b.i].values())))
+
+
+# print "\nSafety check:"
+# print accent_p(B[7],S[1])
+# print duration_p(B[7],S[0])
+
+# Two emission functions
+# xpath = viterbi.viterbi(S,B,T,start_p,accent_p,duration_p)
+# One emission function
+xpath = viterbi.viterbi(S,B,T,start_p, accent_p)
+
+print "\nAnd they said, in great unison, that The Path shalt be:"
+
+sounder = Sounder(32)
+sendlist = [(-1,1,b) for b in range(0,32)]
+for x in xpath:
+    print x
+    # print "Hidden state, transition values ",T[x.i]
+    sendlist[x.origin] = (ra.randint(60,80),x.duration,x.origin)
+
+print sendlist
+sounder.set_notes(sendlist)
+sounder.send_notes()
+sounder.close()
+
---
+++ rhythm32/viterbi.py
@@ -0,0 +1,134 @@
+'''
+    Viterbi implementation modified for an HMM with two emissions,
+    one occuring over transitions (edges) and one from a state.
+
+    Based on algorithm on U{en.wikipedia.org<http://en.wikipedia.org/wiki/Viterbi_algorithm>}. 
+'''
+import numpy
+
+DEBUG=False
+
+def dprint(s=""):
+    if DEBUG:
+        print "<DEBUG>",s
+
+def viterbi(observed, B, T, start_p, p, q=None):
+    '''
+        Run the Viterbi algorithm against our
+        rhythm model which is a Mealy type HMM.
+
+        @param B: the set of the 32 hidden states (the beats)
+        @type B: list
+        @param p: the duration emission probability for each state
+        @type p: numpy.matrix
+        @param observed: set of observed syllables
+        @type observed: list
+        @param T: transition matrix for hidden states
+        @type T: numpy.matrix
+        @param start_p: start probabilities for the states
+        @type start_p: list of probabilities
+        @param q: the accent emission probability for each state.
+                    Default is None.
+        @type q: numpy.matrix
+    '''
+    o_len = len(observed)
+
+    T1 = numpy.matrix( [ [y for y in numpy.zeros(o_len)] for x in numpy.zeros(len(B)) ] )
+    T2 = numpy.matrix( [ [0] * o_len for _ in [0]*len(B) ] )
+
+    dprint("For observations %s: %s \n"%(0, observed[0]))
+    for b in B:
+        if q is None:
+            T1[b.i,0] = (start_p[b.i] *
+                    p(b, observed[0])) 
+            dprint("Highest probability for state %s: %s"%(b,T1[b.i,0]))
+        elif q is not None:
+            raise RuntimeError
+            # T1[b.i,0] = (start_p[b.i] *
+            #         p(b, observed[0]) *
+            #         q(b, observed[0]))
+            # dprint(T1[b.i,0])
+        else:
+            raise RuntimeError("What is wrong with q?")
+        T2[b.i,0] = 0
+    dprint()
+    for t in range(1,o_len):
+        dprint("For observations %s: %s "%(t, observed[t]))
+        for b in B:
+            (pk, k) = transition_max_k(t, T1, b, B, T, observed, p, q)
+            T1[b.i,t] = pk
+            T2[b.i,t] = k.i
+        
+    #pick best last observation
+    (zp, sz) = max([(T1[k.i,o_len-1],k) for k in B])
+
+    #initialize path vector
+    x = [0] * o_len
+    
+    # dprint("T2 matrix with possible paths: %s\n"%T2)
+    #backtrack using T2
+    x[o_len-1] = B[sz.i]
+
+    dprint("Most likely last (t=%s) state for optimal path is %s (p=%s)"%(o_len-1,x[o_len-1],zp))
+    for t in range(o_len-1,0,-1):
+        x[t-1] = B[T2[x[t].i,t]]
+        dprint("Most likely state at t=%s for optimal path is %s (p=%s)"%(t-1,x[t-1],T1[x[t-1].i,t-1]))
+    return x
+
+def transition_max_k(j, T1, b, B, T, observed, p, q):
+    '''
+    Returns the next state which maximizes
+    probability of transition to that state and
+    emission probabilities.
+    Returns a tuplet with index of state as well 
+    as value of probability.
+
+    @param j: the time for which the emission
+                probability is calculated. Is used
+                to index T1.
+    @type j: int
+    @param T1: 2D matrix; where first index, i, is
+                state index and second, j, is time of 
+                emission. Elements give probability
+                of output j being emitted by state i. 
+    @type T1: numpy.matrix
+    @param b: The state which you are currently in.
+    @type b: BeatState.
+    @param B: the set of the 32 hidden states (the beats)
+    @type B: list
+    @param T: transition matrif for hidden states
+    @type T: numpy.matrix
+    @param observed: set of observed syllables
+    @type observed: list
+    @param p: the duration emission probability for each state
+    @type p: numpy.matrix
+    @param q: the accent emission probability for each state.
+                Default is None.
+    @type q: numpy.matrix
+
+    @return: tuplet with two values, first being max 
+                probability and second being index of state
+                that gives max probability.
+    @rtype: tuples
+    '''
+    dprint("Finding likely hidden state for time %s to state %s\n"%(j,b))
+    if q is None:
+        tmpmax = -1
+        tmpk = -1
+        for k in B:
+            # dprint("\n%s\n%s\n%s\n"%
+            #     (T1[k.i,j-1],
+            #     T[k.i,b.i],
+            #     p(b,observed[j])))
+            val=T1[k.i,j-1] * T[k.i,b.i]*p(b,observed[j])
+            if val>tmpmax:
+                tmpmax = val
+                tmpk = k
+        dprint("MAX prob to %s for time %s given by state %s with %s\n---\n"%(b,j,tmpk,tmpmax))
+        return (tmpmax, tmpk)
+        # return max([(T1[k.i,j-1] * T[k.i,b.i]*p(b,observed[j]),k) for k in B])
+    elif q is not None:
+        raise RuntimeError
+        # return max([(T1[k.i,j-1] * T[k.i,b.i]*p(b,observed[j])*q(b,observed[j]),k) for k in B])
+    else:
+        raise RuntimeError("What is wrong with q?")
