f94f3b20b945c18281d29643b1ae9e2494c30de6
parhamfh <parhamfh@kth.se>
2013-01-23 15:19:35 +0900

    Updated test.py to an actual run-script, run.py

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 #!/usr/local/bin/python
 
+import argparse
+
 from skald import Skald
-s=Skald()
+
+parser = argparse.ArgumentParser(description='Run and calculate the rhythm model for Skald.')
+parser.add_argument('-m','--model', nargs='?', dest='model_choice', default='R',
+                    choices=['R','H'],
+                   help="the HMM model you wish to run. R for rhythm model, H for health model. (default: Skald's (R)hythm model)")
+
+args = parser.parse_args()
+if args.model_choice == 'R':
+    s = Skald()
+elif args.model_choice == 'H':
+    s = Skald(health_model=True)
+else:
+    raise RuntimeError("Unknown choice of model. (should not be possible to get this message)")
+# s=Skald()
