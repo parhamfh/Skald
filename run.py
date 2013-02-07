c61f60e4702c0954eed668bca0292394dc2a7864
parhamfh <parhamfh@kth.se>
2013-02-07 21:05:30 +0900

    added option to run mock data
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- run.py
+++ run.py
@@ -10,17 +10,19 @@ parser.add_argument('-m','--model', nargs='?', dest='model_choice', default='R',
                     help="the HMM model you wish to run. R for rhythm model, H for health model. (default: Skald's (R)hythm model)")
 parser.add_argument('-n','--no-score', dest='no_score', action='store_true',
                     help="Do not generate a lilypond score.")
+parser.add_argument('-M','--mock', dest='mock', action='store_true',
+                    help="Mock input")
 #parser.add_argument('-t',choices=['input','transcribing']
                     # help='Test only specified functionality/module.')
 
 args = parser.parse_args()
 
 if args.model_choice == 'R':
-    s = Skald()
-    s.run_model(no_score=args.no_score)
+    s = Skald(mock = args.mock)
+#    s.run_model(no_score=args.no_score)
     
 elif args.model_choice == 'H':
-    s = Skald(health_model=True)
+    s = Skald(health_model=True, mock = args.mock)
 
 else:
     raise RuntimeError("Unknown choice of model. (should not be possible to get this message)")
