7a1e691dad95f2e8d5670102b22f3908213ebe75
parhamfh <parhamfh@kth.se>
2013-02-13 02:05:35 +0900

    Finished abstraction work on BeatPathSet
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- skald/hmm/model/rhythm/elements.py
+++ skald/hmm/model/rhythm/elements.py
@@ -47,39 +47,63 @@ class BeatPair(HmmModelHiddenState):
     def __repr__(self):
         return "{0}{3}({1},{2})".format('B', self.orig, self.dest, self.i)
 
-class BeatPath():
+class BeatPath(object):
         '''
         A list that represents a path of Beats as calculated by the HMM.
-        each entry in the list is a BeatPair. 
+        each entry in the list is a BeatPair.
+        
+        Due to the way BeatPathSet wraps this class and access to it, this
+        will never have to be explicitly instantiated for the BeatPathSet.
         '''
-        def __init__(self, list_of_beat_pairs = []):
+        def __init__(self, idx, list_of_beat_pairs = []):
             self._path = list_of_beat_pairs
+            self._index = idx 
         
         def set_list(self, value):
             self.path = value
 
+        def __iter__(self):
+            return self.path_gen()
+
+        def path_gen(self):
+            for i in self.path:
+                yield i
+
+        @property
+        def i(self):
+            return self._index
+        
         @property
         def path(self):
             return self._path
 
         def __repr__(self):
-            return '{0}'.format(self.path)
+            return 'BeatPath #{0}'.format(self.path)
 
 class BeatPathSet(object):
     '''
     Set that contains BeatPaths as calculated by the HMM.
     Each BeatPath corresponds to one line of text from the user.
+    
+    Upon calling BeatPathSet[key] (with or without assignment) a 
+    BeatPath will be instantiated if it is the first time referencing that key.
+    
+    If it also is an assignment, the BeatPath's u{set_value} method will be
+    called.
+    
+    Due to the way BeatPathSet wraps BeatPath objects and access to them, the 
+    BeatPath object will never have to be explicitly instantiated for the class.
     '''
 
     def __init__(self, number_of_beatpaths):
-        self.number_of_bp = number_of_beatpaths
+        self._number_of_bp = number_of_beatpaths
         self._paths = [None] * number_of_beatpaths
     
     def __getitem__(self, key):
         if key < self.number_of_bp and key >= 0:
             if self.paths[key] is None:
                 #print '__getitem__: Creating BeatPath object for index %s'%key
-                self.paths[key] = BeatPath()
+                self.paths[key] = BeatPath(key)
             
             return self.paths[key]
         else:
@@ -90,16 +114,20 @@ class BeatPathSet(object):
         if key < self.number_of_bp and key >= 0:
             if self.paths[key] is None:
                 #print '__setitem__: Creating BeatPath object for index %s'%key
-                self.paths[key] = BeatPath()
+                self.paths[key] = BeatPath(key)
             
             self.paths[key].set_list(value)
         else:
             #print "BeatPathSet.__setitem__: Index key out of bounds."
             raise IndexError("BeatPathSet.__setitem__: Index key out of bounds.")
-
+    @property
+    def number_of_bp(self):
+        return self._number_of_bp
+    
     @property
     def paths(self):
         return self._paths
+
 class Syllable(HmmModelObservation):
     '''
     Output/emission.
--- skald/main_skald.py
+++ skald/main_skald.py
@@ -8,7 +8,7 @@ import sys
 
 from skald.hmm import Hmm
 from skald.hmm.model.rhythm import RhythmModel
-#from skald.hmm.model.rhythm.elements import Syllable
+from skald.hmm.model.rhythm.elements import BeatPathSet, BeatPair
 
 from skald.lilypond.ponder import Ponder
 
@@ -73,13 +73,15 @@ class Skald(object):
         if isinstance(self.observations, SyllableSet):
             o_len = len(self.observations)
             self.hmm = [None] * o_len
-            self.path = [None] * o_len
+            self.beat_paths = BeatPathSet(o_len)
 
             for i in range(o_len):
                 self.hmm[i] = Hmm(RhythmModel, self.observations[i])
                 self.path[i] = self.hmm[i].find_most_likely_state_seq()
                 self.hmm[i].print_path()
                 self.hmm[i].model.print_beats(self.path[i], self.observations[i])
+                # TODO: fix this, very ugly!
+                BeatPair._reset_object_counter()
                 
             if not no_score:
                 self.generate_lilypond_score(self.path, self.observations, 32*o_len)
@@ -93,7 +95,8 @@ class Skald(object):
                 self.generate_lilypond_score(self.path, self.observed, 32)
     
     def generate_lilypond_score(self, xpath, observations, num_beats):
-        pon = Ponder(xpath,num_beats/16, observations)
+#        pon = Ponder(xpath,num_beats/16, observations)
+        pon = Ponder(xpath, observations)
         pon.make_ly_file()
     
     def send_to_pd(self, xpath, num_beats):
