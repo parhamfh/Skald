71fe9ad410d8631a764e79920dd4e83a35a1f11c
parhamfh <parhamfh@kth.se>
2013-02-13 03:06:29 +0900

    Fixed bug with assignment to property in newstyle class.
    BeatPath was previously not inheriting from object and was therefor not a newstyle class. In oldstyle classes you can assign to properties without using property setters. shameful.
    
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- skald/hmm/model/rhythm/elements.py
+++ skald/hmm/model/rhythm/elements.py
@@ -77,6 +77,10 @@ class BeatPath(object):
         def path(self):
             return self._path
 
+        @path.setter
+        def path(self,value):
+            self._path = value
+
         def __repr__(self):
             return 'BeatPath #{0}'.format(self.path)
 
@@ -88,7 +92,7 @@ class BeatPathSet(object):
     Upon calling BeatPathSet[key] (with or without assignment) a 
     BeatPath will be instantiated if it is the first time referencing that key.
     
-    If it also is an assignment, the BeatPath's u{set_value} method will be
+    If it also is an assignment, the BeatPath's u{set_list} method will be
     called.
     
     Due to the way BeatPathSet wraps BeatPath objects and access to them, the 
