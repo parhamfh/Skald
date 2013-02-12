e532b743272352dc9738abe2c15f64080c8cc2b5
parhamfh <parhamfh@kth.se>
2013-02-12 19:44:38 +0900

    Introduced BeatPath/BeatPathSet class to abstract.
    Abstracted the list manipulation so that we can be more flexible when dealing with lilypond score generation. Old way only one list of BeatPaths elements was given as input, but now we want a list of lists of BeatPaths, thus this approach. Better safe than sorry.
    
    
    
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- skald/hmm/model/rhythm/elements.py
+++ skald/hmm/model/rhythm/elements.py
@@ -41,6 +41,59 @@ class BeatPair(HmmModelHiddenState):
     def __repr__(self):
         return "{0}{3}({1},{2})".format('B', self.orig, self.dest, self.i)
 
+class BeatPath():
+        '''
+        A list that represents a path of Beats as calculated by the HMM.
+        each entry in the list is a BeatPair. 
+        '''
+        def __init__(self, list_of_beat_pairs = []):
+            self._path = list_of_beat_pairs
+        
+        def set_list(self, value):
+            self.path = value
+
+        @property
+        def path(self):
+            return self._path
+
+        def __repr__(self):
+            return '{0}'.format(self.path)
+
+class BeatPathSet(object):
+    '''
+    Set that contains BeatPaths as calculated by the HMM.
+    Each BeatPath corresponds to one line of text from the user.
+    '''
+
+    def __init__(self, number_of_beatpaths):
+        self.number_of_bp = number_of_beatpaths
+        self._paths = [None] * number_of_beatpaths
+    
+    def __getitem__(self, key):
+        if key < self.number_of_bp and key >= 0:
+            if self.paths[key] is None:
+                #print '__getitem__: Creating BeatPath object for index %s'%key
+                self.paths[key] = BeatPath()
+            
+            return self.paths[key]
+        else:
+            #print "BeatPathSet.__getitem__: Index key out of bounds."
+            raise IndexError("BeatPathSet.__getitem__: Index key out of bounds.")
+    
+    def __setitem__(self, key, value):
+        if key < self.number_of_bp and key >= 0:
+            if self.paths[key] is None:
+                #print '__setitem__: Creating BeatPath object for index %s'%key
+                self.paths[key] = BeatPath()
+            
+            self.paths[key].set_list(value)
+        else:
+            #print "BeatPathSet.__setitem__: Index key out of bounds."
+            raise IndexError("BeatPathSet.__setitem__: Index key out of bounds.")
+
+    @property
+    def paths(self):
+        return self._paths
 class Syllable(HmmModelObservation):
     '''
     Output/emission.
