94bd1baa225535eb1b400eabe9be4906d8b9081b
parhamfh <parhamfh@kth.se>
2013-02-20 19:43:31 +0900

    Added warnings to signal that package is work in progress!
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- skald/misc/mock.py
+++ skald/misc/mock.py
@@ -1,13 +1,20 @@
 '''
 Created on Feb 8, 2013
 
+THIS CLASS IS CURRENTLY NOT WORKING PLEASE SEE
+
+http://stackoverflow.com/questions/14755754/pass-keyword-argument-only-to-new-and-never-further-it-to-init/
+
+Thanks!
+
 @author: parhamfh
 '''
+
+raise RuntimeError('Module mock is not currently working!')
 
 class Mockable(object):
     
     def __new__(cls, *args, **kwargs):
-        
         if kwargs.pop('mock', None):
             mock_cls = eval('{0}{1}'.format('Mock',cls.__name__))
             print mock_cls
