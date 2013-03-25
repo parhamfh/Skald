7e593562571a3949f42b04674cca789c6842cd07
parhamfh <parhamfh@kth.se>
2013-03-25 18:56:20 +0900

    timing execution of Skald
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
 import argparse
+import time
 
 from skald import Skald
 
@@ -23,6 +24,9 @@ args = parser.parse_args()
 print "------------------------------------------------"
 print "|                 S K A L D                    |"
 print "------------------------------------------------"
+
+start_time = time.time()
+
 if args.model_choice == 'R':
     print "Generate musical score: {0}".format("Yes" if not args.no_lilypond else "No")
     print "Generate output for Orpheus: {0}".format("Yes" if not args.no_orpheus else "No")
@@ -37,4 +41,6 @@ elif args.model_choice == 'H':
 else:
     raise RuntimeError("Unknown choice of model. (should not be possible to get this message)")
 
+end_time = time.time()
+print "Execution time: {1} ({0})".format(end_time - start_time,round(end_time - start_time, 6))
 # s=Skald()
