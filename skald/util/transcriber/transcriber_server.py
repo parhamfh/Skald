49efeaf5a2a7b986f10272c632b03b98873b99b7
Pärham Fazelzadeh Hashemi <parhamfh@csc.kth.se>
2013-06-20 01:34:16 +0200

    TranscriberServer now calls script to phonetie text, on KTH machine

--- skald/util/transcriber/transcriber_server.py
+++ skald/util/transcriber/transcriber_server.py
@@ -1,5 +1,8 @@
+# coding: utf8
 import re
 import sys
+import subprocess
+
 class TranscriberServer(object):
 
     def __init__(self):
@@ -9,8 +12,8 @@ class TranscriberServer(object):
     def transcribe(self, message):
         transcribed = []
         for line in message.split('\n'):
-            lst = re.findall(u"[\w']+", message)
-            
+	    print u"Processing line: \"{0}\"\n...".format(line)
+	    lst = re.findall("[\w']+", line, re.UNICODE)
             for i in range(0,len(lst)):
                 p = self.phonetize(lst[i])
                 s = self.syllabify(p)
@@ -19,19 +22,30 @@ class TranscriberServer(object):
         return transcribed
 
     def phonetize(self, word):
-        return "phonem{0}".format(word)
+    	sys.path.append("/afs/nada.kth.se/home/2/u1weowl2/kurser/exjobb/Skald/scripts/")
+	phonetize_proc = subprocess.Popen(['test_tts.tcl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	phonem, err = phonetize_proc.communicate(word.encode(encoding='utf8'))
+        return u"{0}".format(phonem.decode(encoding='utf8').rstrip('\n'))
         # run local files
 
     def syllabify(self, phonetic):
-        return "syllables{0}".format(phonetic)
+        return "s"
+
         # run local files
 if __name__ == '__main__':
 
     t = TranscriberServer()
-    print t.transcribe(sys.argv[1].decode(encoding='utf8'))
-    s = 'de ljuva orden i mitt liv'+\
-        'behöver inte säga att det är över'+
-        'inte heller att det tar slut'+
-        'utan ljud, utanför bild'+
-        'det kommer ta tid, men sen är jag din'+
-        'på din brits'
+    # print t.transcribe(sys.argv[1].decode(encoding='utf8'))
+    s = 'En liten gåva till D.\n'\
+    	+'de "ljuva" orden i mitt liv\n'\
+        +'behöver inte säga att det är över\n'\
+        +'inte heller att det tar slut\n'\
+        +'utan ljud, utanför bild\n'\
+	+'det kommer ta tid, men sen är jag din\n'\
+        +'på din brits'
+    s = unicode(s, encoding='utf8')
+    response = t.transcribe(s)
+    for line in response:
+    	print '\n',"|".join(map(lambda (x,y,z):x, line))
+	for (a,b,c) in line:
+		print u'    {0} : {1}'.format(a,b)
