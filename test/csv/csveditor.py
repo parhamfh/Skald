8c74a27c841651ff1fdcea44f9a41fe3a542e43b
parhamfh <parhamfh@kth.se>
2013-10-19 19:44:39 +0200

    WIP: Progress on parsing samples.pk and modifying it, need to fix the final stage of outputting modified sample.pk

--- test/csv/csveditor.py
+++ test/csv/csveditor.py
@@ -2,7 +2,10 @@
 
 # coding: utf8
 
-import csv
+import json
+import collections
+
+import unicodecsv
 
 def is_int(int_candidate):
     try:
@@ -19,7 +22,7 @@ def has_second_part(candidate):
         return False
 
 with open('sample.pk') as csv_f:
-    red = csv.reader(csv_f, delimiter=',')
+    red = unicodecsv.UnicodeReader(csv_f, delimiter=',')
     dic = {}
 
     for row in red:
@@ -67,7 +70,31 @@ with open('sample.pk') as csv_f:
         else:
             fin_dic[key] = dic[key]
 
-    for key in sorted(fin_dic.keys()):
-        print key
-        print fin_dic[key]
-        print '\n==================='
+    # for key in sorted(fin_dic.keys()):
+    #     print key
+    #     print fin_dic[key]
+    #     print '\n==================='
+
+    # Dump our crap to make the orpheus_template.json
+    json_dump = json.dumps(fin_dic, sort_keys=True, indent=4)
+    print json_dump
+
+    # Check that we can read in the json and put it into csv format
+    orp_json = json.loads(json_dump)
+    for shit in orp_json:
+        print shit, type(orp_json[shit]), len(orp_json[shit])
+
+    # Write back the loaded JSON template in CSV with changes 
+    # we want orpheus to respect included
+
+    with open('csvsampleexample.pk','wb') as csvsamplepk:
+        shitwriter = unicodecsv.UnicodeWriter(csvsamplepk, delimiter=',')
+        # For all the divs, merge the keys in the dict with the div number
+        # To create the crappy <div number>:<key>,<value> format
+        for crap in orp_json:
+            assert False
+            shitwriter.writerow([crap,orp_json[crap]])
+
+    # Read in the csv again and jsonify check the keys against the json orp_json
+    # Here below
+    assert False
