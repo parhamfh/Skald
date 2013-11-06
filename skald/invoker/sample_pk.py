4aeb6c5cc5f79455bc44d08e27d6d11c696aba57
parhamfh <parhamfh@kth.se>
2013-11-06 02:39:02 +0100

    WIP: Generating sample.pk from template and user choices.
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- skald/invoker/sample_pk.py
+++ skald/invoker/sample_pk.py
@@ -1,21 +1,27 @@
+# coding: utf8
+
+import skald.util.assets as assets
 
 class SamplePKFormatter(object):
 
+    ORPHEUS_JSON_TEMPLATE = 'orpheus_template.json'
+    
     def __init__(self, settings, number_of_verses):
         self.settings = settings
         self.number_of_verses = number_of_verses
 
     def generate_sample_pk(self, f=None):
-        if f is None:
-            f = open('sample.pk', 'w')
-        with f:
-            f.write('div,%s\n'%self.number_of_verses)
+        self._read_in_json_template()
+        # if f is None:
+        #     f = open('sample.pk', 'w')
+        # with f:
+        #     f.write('div,%s\n'%self.number_of_verses)
 
     ## TAKE stuff from csveditor.py
 
     def _read_in_json_template(self):
-        pass
-
+        f = assets.get_my_file(self.ORPHEUS_JSON_TEMPLATE)
+        
     def _settings_to_json(self):
         pass
 
