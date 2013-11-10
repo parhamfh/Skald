7b7de928e3a1d08a4c074090e1e93897f4d3916b
parhamfh <parhamfh@kth.se>
2013-11-10 01:16:29 +0100

    import datatype util
    Signed-off-by: parhamfh <parhamfh@kth.se>

--- skald/util/__init__.py
+++ skald/util/__init__.py
@@ -2,3 +2,4 @@ from parser import InputParser
 from syllabify import Syllabifyer, SyllableSet
 from transcriber import PhoneticTranscriber
 from validator import InputValidator
+from datatype_ops import *
--- skald/util/assets.py
+++ skald/util/assets.py
@@ -22,6 +22,7 @@ def get_my_file(filename, mode=None):
     # join paths
     # Set env for assets folder and use that
     
+    print 'module name is : ', module_name
     p = os.path.join(ASSETS_FOLDER, module_name, filename)
 
     # Mode
