b9d72b6be2011807649cfed0179771a5392003d4
Pärham Fazelzadeh <parhamfh@kth.se>
2014-10-02 01:02:11 +0200

    updated config

--- skald/config/__init__.py
+++ skald/config/__init__.py
@@ -2,18 +2,33 @@
 import os
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
-SKALD_DIRECTORY = os.path.abspath(os.path.join(CURRENT_DIRECTORY,
-                                                    '../../'))
+
+## SKALD
+
+SKALD_DIRECTORY = os.path.abspath(os.path.join(CURRENT_DIRECTORY, '../../'))
 SKALD_SRC_DIRECTORY = os.path.join(SKALD_DIRECTORY, 'skald')
+
 SKALD_ASSETS_DIRECTORY = os.path.join(SKALD_DIRECTORY,
     'resources',
     'assets')
 
 skald = {
-    'directory' : SKALD_DIRECTORY,
+    'directory': SKALD_DIRECTORY,
     'src': SKALD_SRC_DIRECTORY,
-    'assets' : SKALD_ASSETS_DIRECTORY,
-    'debug' : True,
-    'logging' : 'debug',
-    'orpheus_format' : 'stdout'
+    'assets': SKALD_ASSETS_DIRECTORY,
+    'debug': True,
+    'logging': 'debug',
+    'orpheus_format': 'stdout'
+}
+
+## ORPHEUS
+
+ORPHEUS_DIRECTORY = os.path.join(SKALD_DIRECTORY,'contrib','orpheus-skald')
+orpheus = {
+    'directory': ORPHEUS_DIRECTORY,
+    'bin': os.path.join(ORPHEUS_DIRECTORY,'orpheus_v3.4','bin'),
+    'output': {
+        'folder': os.path.join(ORPHEUS_DIRECTORY,'out'),
+        'format': 'stdout',
+    }
 }
